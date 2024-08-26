eb91c0868bc64313ca06589e8a671bd6844902f2
Bl3nder1 <ble3nder.rt@mail.ru>
2024-08-27 06:37:22 +0700

    Delete time_zone.py

--- time_zone.py
+++
@@ -1,5 +0,0 @@
-'''with open('Viktorina.json', 'r', encoding='utf-8') as f:
-    obj = json.load(f)
-    obj[cb[-1]]["all"] = 50
-    with open('Viktorina.json', 'w') as f:
-        json.dump(obj, f)'''
